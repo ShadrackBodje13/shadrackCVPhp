fa77acadcf4d396b4fbc08b431afae672665b842
ShadrackBodje13 <56391856+ShadrackBodje13@users.noreply.github.com>
2020-05-16 12:22:46 +0200

    update5 update connexion.php
    connexion authentification verification

--- connexion.php
+++ connexion.php
@@ -22,6 +22,46 @@
   <link href="css/resume.min.css" rel="stylesheet">
 
 </head>
+<?php
+  // on teste si le visiteur a soumis le formulaire de connexion
+if (isset($_POST['entrez'])) {
+  if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['mdp']) && !empty($_POST['mdp']))) {
+
+      $base = mysql_connect ('localhost', 'root', '');
+      mysql_select_db ('esv', $base);
+
+      // on teste si une entrée de la base contient ce couple login / pass
+      $sql = 'SELECT count(*) FROM user WHERE login="'.mysql_escape_string($_POST['login']).'" AND mdp="'.mysql_escape_string($_POST['mdp']).'"';
+      $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+      $data = mysql_fetch_array($req);
+
+      mysql_free_result($req);
+      mysql_close();
+
+      // si on obtient une réponse, alors l'utilisateur est un membre
+      if ($data[0] == 1) {
+          session_start();
+          $_SESSION['login'] = $_POST['login'];
+          header('Location: liste.php');
+          exit();
+      }
+      // si on ne trouve aucune réponse, le visiteur s'est trompé soit dans son login, soit dans son mot de passe
+      elseif ($data[0] == 0) {
+          $erreur = 'Compte non reconnu. <br/> <a href="index.php">Retour au formulaire</a>';
+      }
+      // sinon, alors la, il y a un gros problème
+      else {
+          $erreur = 'Problème dans la base de données : plusieurs membres ont les mêmes identifiants de connexion.';
+      }
+  }
+  else {
+    $erreur = 'Au moins un des champs est vide.';
+  }
+}
+   
+  
+?>
+
 <body id="page-top">
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
